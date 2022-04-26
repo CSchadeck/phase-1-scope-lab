33949542dbf7915b9317de30fcb7cf0cf2c10909
Corey Schadeck <corey@schadeck.com>
2022-04-26 17:18:04 -0400

    commit tree 2b11f00303ce70f3fcee7e2bfb9eddc9b2bdfbba

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+var customerName="bob" 
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    return "not bob"
+}
+    
+
+ 
